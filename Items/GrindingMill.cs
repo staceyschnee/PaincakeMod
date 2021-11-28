1a6721fadddba37f2071e90b8a71ccb2e457691c
stacey <stacey@schnee.com>
2021-11-28 17:45:26 -0500

    Add GrindingMill for creation of flour

---
+++ Items/GrindingMill.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+using Terraria.GameContent.Creative;
+using PaincakeMod.Tiles;
+
+namespace PaincakeMod.Items
+{
+	class GrindingMill : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("Grinding Mill");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 46;
+			Item.height = 34;
+			Item.value = 350;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.rare = ItemRarityID.Green;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+			Item.consumable = true;
+			Item.useTurn = true;
+			Item.maxStack = 99;
+			Item.createTile = ModContent.TileType<GrindingMillTile>();
+		}
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.Wood, 4);
+			recipe.AddIngredient(ItemID.StoneBlock, 10);
+			recipe.AddIngredient(ItemID.IronBar, 2);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+	}
+}
---
+++ Tiles/GrindingMillTile.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Terraria.ModLoader.Utilities;
+using Terraria.GameContent.Creative;
+using Terraria.ObjectData;
+using PaincakeMod.Items;
+
+
+namespace PaincakeMod.Tiles
+{
+	class GrindingMillTile : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileObsidianKill[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+			TileObjectData.newTile.Origin = new Point16(0, 1);
+			TileObjectData.newTile.LavaDeath = false;
+			TileObjectData.newTile.DrawYOffset = 2;
+			TileObjectData.addTile(Type);
+
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Grinding Mill");
+			AddMapEntry(new Color(200, 200, 200), name);
+			 
+			//Can't use this since texture is vertical
+			AnimationFrameHeight = 34;
+		}
+
+		public override void AnimateTile(ref int frame, ref int frameCounter)
+		{
+			
+			// Spend 9 ticks on each of 6 frames, looping
+			if (++frameCounter >= 9) {
+				frameCounter = 0;
+				frame = ++frame % 4;
+			}
+
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			Item.NewItem(i * 16, j * 16, 16, 48, ModContent.ItemType<GrindingMill>());
+		}
+
+	}
+}
