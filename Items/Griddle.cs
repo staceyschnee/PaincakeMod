c18f1b7adbb13fc971cff16d03bb545902ea4708
stacey <stacey@schnee.com>
2021-11-28 19:55:56 -0500

    Add Griddle for paincakes crafting

---
+++ Items/Griddle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+using Terraria.GameContent.Creative;
+using PaincakeMod.Tiles;
+
+namespace PaincakeMod.Items
+{
+	class Griddle : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("Used for special crafting");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 32;
+			Item.height = 24;
+			Item.value = 650;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.rare = ItemRarityID.Green;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+			Item.consumable = true;
+			Item.useTurn = true;
+			Item.maxStack = 999;
+			Item.createTile = ModContent.TileType<GriddleTile>();
+		}
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+				.AddIngredient(ItemID.Torch, 4)
+				.AddIngredient(ItemID.StoneBlock, 6)
+				.AddIngredient(ItemID.IronBar, 10)
+				.AddTile(TileID.WorkBenches)
+				.Register();
+
+			CreateRecipe()
+				.AddIngredient(ItemID.Torch, 4)
+				.AddIngredient(ItemID.StoneBlock, 6)
+				.AddIngredient(ItemID.LeadBar, 10)
+				.AddTile(TileID.WorkBenches)
+				.Register();
+
+		}
+	}
+}
---
+++ Tiles/GriddleTile.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Terraria.ModLoader.Utilities;
+using Terraria.GameContent.Creative;
+using Terraria.ObjectData;
+using PaincakeMod.Items;
+
+
+namespace PaincakeMod.Tiles
+{
+	class GriddleTile : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileObsidianKill[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+			TileObjectData.newTile.LavaDeath = false;
+			TileObjectData.addTile(Type);
+
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Griddle");
+			AddMapEntry(new Color(200, 200, 200), name);
+
+			//Can't use this since texture is vertical
+			//AnimationFrameHeight = 34;
+		}
+
+#if false
+		public override void AnimateTile(ref int frame, ref int frameCounter)
+		{
+
+			// Spend 9 ticks on each of 4 frames, looping
+			if (++frameCounter >= 9)
+			{
+				frameCounter = 0;
+				frame = ++frame % 4;
+			}
+
+		}
+#endif
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			Item.NewItem(i * 16, j * 16, 32, 32, ModContent.ItemType<Griddle>());
+		}
+
+	}
+}
